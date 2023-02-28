2db86b3ce93fa0277147776f24f73b5d2138199f
Elisa Kortela <kortela@protonmail.com>
2023-02-28 15:19:11 +0200

    Työilmoituksen esityksen päivitys
    - Paljon refaktorointia helpottamaan jatkokehitystä
    - Napit reagoivat painallukseen visuaalisesti
    - "Lue lisää" -toiminto työn kuvaukseen
    - Päivämäärien muotoilu kuntoon
    - Kaikki ei-pakolliset arvot luodaan nyt ehdollisesti
    - Tyylit siirretty omaan tiedostoon styles.jsx kaikkien käytettäväksi

--- navigation/jobAdvertisementScreen.jsx
+++ navigation/jobAdvertisementScreen.jsx
@@ -1,183 +1,274 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import Divider from 'react-native-divider';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { format } from 'date-fns';
+import ViewMoreText from 'react-native-view-more-text';
 import jobAd from "./dummyAdvertisement";
+import Styles from '../styles';
 
-const styles = StyleSheet.create({
-  containerBright: {
-    backgroundColor: 'white',
-    paddingHorizontal: 10,
-    paddingBottom: 20,
-    paddingTop: 10
-  },
-  containerDim: {
-    backgroundColor: 'azure',
-    paddingHorizontal: 10,
-    paddingBottom: 20,
-    paddingTop: 10
-  },
-  row: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    paddingVertical: 3
-  },
-  button: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    borderRadius: 20,
-    backgroundColor: 'mediumaquamarine',
-    alignSelf: 'flex-start',
-    marginHorizontal: '1%',
-    marginBottom: 6,
-    minWidth: '33%',
-    textAlign: 'center',
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: '500',
-    color: 'black',
-    textAlign: 'center'
-  },
-  h1: {
-    textAlign: 'left',
-    marginBottom: 10,
-    fontSize: 24,
-  },
-  h2: {
-    textAlign: 'left',
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  icon: {
-    paddingEnd: 10,
-    textAlignVertical: 'center',
-  },
-  iconButton: {
-    textAlignVertical: 'center',
-    padding: 5
-  },
-  rowButton: {
-    paddingTop: 20,
-    alignItems: 'stretch',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between'
-  }
-});
+// Icon property constants
+const iconSize = 15;  // For header details and job details
+const headerIconColor= 'black';
+const jobDetailIconColor = 'mediumaquamarine';
+const buttonIconSize = 30;
+const buttonIconColor = 'black';
+const h2IconSize = 20;
+const h2IconColor = 'mediumaquamarine';
 
+// Header with job title and location
 const HeaderTitle = ({values}) => (
   <View>
-    <Text style={styles.h1}>
-      {values.title}{values.location ? ', ' + values.location : ''}
+    <Text style={Styles.h1}>
+      { values.title }{ values.location ? ', ' + values.location : '' }
     </Text>
   </View>
 )
 
+// Returns formatted timestamp
+const formatTime = (timeStamp) => {
+  return format(new Date(timeStamp), 'dd.MM.yyyy kk:mm')
+}
+
+// Takes in array of strings, returns employment data elemet for header
+const renderHeaderEmploymentData = (employmentData) => {
+  let employmentText = employmentData.join(", ")
+
+  if (employmentText.length === 0) return null
+
+  return (
+    <View style={Styles.row}>
+      <Icon style={Styles.icon} name={'briefcase'} size={iconSize} color={headerIconColor}/>
+      <Text>
+        { employmentText }
+      </Text>
+    </View>
+  )
+}
+
+// Header info section with due date, employment details, employer organization details
 const HeaderInfo = ({values}) => (
   <View>
-    <Text style={styles.row}>Hakuaika päättyy {values.publicationEnds}</Text>
-    <View style={styles.row}>
-      <Icon style={styles.icon} name={'briefcase'} size={10} color={'black'}/>
-      <Text>
-        {values.employmentType ? values.employmentType : ''}{values.employment ? ', ' + values.employment : ''}
-      </Text>
-    </View>
-    <View style={styles.row}>
-      <Icon style={styles.icon} name={'institution'} size={10} color={'black'}/>
-      <Text>
-        {values.organization ? values.organization : ''}
-      </Text>
-    </View>
+    <Text style={Styles.row}>Hakuaika päättyy { formatTime(values.publicationEnds) }</Text>
+    { renderHeaderEmploymentData([values.employmentType, values.employment]) }
+    { values.organization ? 
+      <View style={Styles.row}>
+        <Icon style={Styles.icon} name={'institution'} size={iconSize} color={headerIconColor}/>
+        <Text>
+          { values.organization }
+        </Text>
+      </View> 
+    : null }   
   </View>
 );
 
+// Heart button
+const HeartButton = () => {
+  const [heartButtonState, setHeartButtonState] = useState({color: 'black', name: 'heart-o'})
+
+  // Pressing the heart button
+  const onHeartButtonPress = () => {
+    if (heartButtonState.color === 'black') {
+      setHeartButtonState({color: 'mediumaquamarine', name: 'heart'})
+    } else {
+      setHeartButtonState({color: 'black', name: 'heart-o'})
+    }
+  }
+
+  return (
+    <TouchableOpacity onPress={ () => onHeartButtonPress() }>
+      <Icon style={Styles.iconButton} size={buttonIconSize} name={heartButtonState.name} color={heartButtonState.color}/>
+    </TouchableOpacity>
+  )
+}
+
+// Share button
+const ShareButton = () => {
+  const onShareButtonPress = () => {
+    // Mitä tapahtuu kun painetaan jakonappia
+  }
+
+  return (
+    <TouchableOpacity onPress={ () => onShareButtonPress() }>
+      <Icon style={Styles.iconButton} size={buttonIconSize} name={'share-alt'} color={buttonIconColor}/>
+    </TouchableOpacity>
+  )
+}
+
+// Apply for job button
+const ApplyForJobButton = () => {
+  const onApplyForJobButtonPress = () => {
+    // Mitä tapahtuu kun painetaan "HAE PAIKKAA" -nappia
+  }
+
+  return (
+    <TouchableOpacity style={Styles.button} onPress={ () => onApplyForJobButtonPress() }>
+      <Text style={Styles.buttonLabel}>HAE PAIKKAA</Text>
+    </TouchableOpacity>
+  )
+}
+
+// Buttons at the bottom of header
 const HeaderButtons = () => (
-  <View style={styles.rowButton}>
+  <View style={Styles.rowButton}>
     <View>
-      <TouchableOpacity style={styles.button} onPress={()=>{}}>
-        <Text style={styles.buttonLabel}>HAE PAIKKAA</Text>
-      </TouchableOpacity>
+      <ApplyForJobButton />
     </View>
-    <View style={styles.row}>
-      <Icon style={styles.iconButton} size={25} name={'heart-o'} color={'black'}/>
-      <Icon style={styles.iconButton} size={25} name={'share-alt'} color={'black'}/>
+    <View style={[Styles.row, {paddingEnd: 30}]}>
+      <HeartButton />
+      <ShareButton />
     </View>
   </View>
 )
 
+// Header containig title, info and buttons
 const HeaderLayout = ({values}) => (
-  <View style={styles.containerBright}>
+  <View style={[Styles.container, Styles.containerBright]}>
     <HeaderTitle values={values} />
     <HeaderInfo values={values} />
     <HeaderButtons />
   </View>
 )
 
+// View more description text
+const renderViewMore = (onPress) => {
+  return(
+    <Text style={Styles.viewToggle} onPress={onPress}>Näytä enemmän</Text>
+  )
+}
+
+// View less description text
+const renderViewLess = (onPress) => {
+  return(
+    <Text style={Styles.viewToggle} onPress={onPress}>Näytä vähemmän</Text>
+  )
+}
+
 // Työkuvausta ei välttämättä ole, pitää ottaa huomioon
-const JobDescription = ({values}) => (
-  <View style={styles.containerDim}>
-    <Text style={styles.h2}>
-      Työn kuvaus
-    </Text>
-    <Text>
-      {values.jobDesc ? values.jobDesc : ''}
-    </Text>
-  </View>
-)
+const JobDescription = ({values}) => {
+  if (!values.jobDesc) return null
 
-// Näistä kaikki ei välttämättömiä, pitää ottaa huomioon
+  return (
+    <View style={[Styles.container, Styles.containerDim]}>
+      <Text style={Styles.h2}>
+        Työn kuvaus
+      </Text>
+      <ViewMoreText
+          numberOfLines={15}
+          renderViewMore={renderViewMore}
+          renderViewLess={renderViewLess}
+          textStyle={{textAlign: 'left'}}
+        >
+        <Text>
+          { values.jobDesc }
+        </Text>
+      </ViewMoreText>
+    </View>
+  )  
+}
+
+// Lista yksityiskohtaisia tietoja työstä
 const JobDetails = ({values}) => (
-  <View style={[{paddingVertical: 20}, styles.containerBright]}>
-    <View style={styles.row}>
-      <Icon style={styles.icon} name={'key'} size={15} color={'mediumaquamarine'}/>
+  <View style={[{paddingVertical: 20}, Styles.container, Styles.containerBright]}>
+    <View style={Styles.row}>
+      <Icon style={Styles.icon} name={'key'} size={iconSize} color={jobDetailIconColor}/>
       <Text>
-        {values.publishingOrganization}
+        { values.publishingOrganization }
       </Text>
     </View>
     <Divider />
-    <View style={styles.row}>
-      <Icon style={styles.icon} name={'play-circle-o'} size={15} color={'mediumaquamarine'}/>
-      <Text>
-        {values.jobDuration ? values.jobDuration : ''}
-      </Text>
-    </View>
+    { values.jobDuration ?
+      <View style={Styles.row}>
+        <Icon style={Styles.icon} name={'play-circle-o'} size={iconSize} color={jobDetailIconColor}/>
+        <Text>
+          { values.jobDuration }
+        </Text>
+      </View>
+    : null }
     <Divider />
-    <View style={styles.row}>
-      <Icon style={styles.icon} name={'euro'} size={15} color={'mediumaquamarine'}/>
+    { values.salary ?
+    <View style={Styles.row}>
+      <Icon style={Styles.icon} name={'euro'} size={iconSize} color={jobDetailIconColor}/>
       <Text>
-        {values.salary ? values.salary : ''}
+        { values.salary }
       </Text>
     </View>
+    : null }
   </View>
 )
 
-// Näistä kaikki ei välttämättömiä, pitää ottaa huomioon
-// Osoite koostuu useasta osasta, joista mikään ei ole pakollinen
+// Render contacts details
+const renderContacts = (contacts) => {
+  if (!contacts) return null;
+
+  return (
+    <View>
+      <View style={Styles.row}>
+        <Icon style={Styles.icon} name={'envelope'} size={h2IconSize} color={h2IconColor}/>
+        <Text style={Styles.h2}>
+          Yhteystiedot
+        </Text>
+      </View>
+      <Text>
+        { contacts }
+      </Text>
+    </View>
+  )
+}
+
+// Returns string with format "Address, postalCode location"
+// Each section is optional
+const renderAddressBlock = (address, postalCode, location) => {
+  if (!address && !postalCode && !location) return null;
+
+  let codeAndLocation;
+  if (postalCode) codeAndLocation = postalCode.concat(' ');
+  if (location) codeAndLocation = codeAndLocation.concat(location);
+  
+  let addressString;
+  if (address && codeAndLocation) {
+    addressString = address.concat(', ', codeAndLocation)
+  } else if (address) addressString = address
+  else addressString = codeAndLocation;
+
+  return (
+    <View>
+      <Text style={{color: 'mediumaquamarine', paddingTop: 10}}>
+        { addressString }
+      </Text>
+    </View>
+  )
+}
+
+// Returns organization details and address
+const renderOrganizationAndAddress = (organizationDesc, addressBlock) => {
+  if (!organizationDesc && !addressBlock) return null;
+
+  return(
+    <View>
+      <View style={Styles.row}>
+        <Icon style={Styles.icon} name={'briefcase'} size={h2IconSize} color={h2IconColor}/>
+        <Text style={Styles.h2}>
+          Työnantaja
+        </Text>
+      </View>
+      <Text>
+        { organizationDesc ? organizationDesc : null }
+      </Text>
+        { addressBlock }
+    </View>
+  )
+}
+
+// Contacts, organization details and address
 const ContactInformation = ({values}) => (
-  <View style={styles.containerDim}>
-    <View style={styles.row}>
-      <Icon style={styles.icon} name={'envelope'} size={20} color={'mediumaquamarine'}/>
-      <Text style={styles.h2}>
-        Yhteystiedot
-      </Text>
-    </View>
-    <Text>
-      {values.contacts ? values.contacts : ''}
-    </Text>
+  <View style={[Styles.container, Styles.containerDim]}>
+    { renderContacts(values.contacts) }
     <Divider />
-    <View style={styles.row}>
-      <Icon style={styles.icon} name={'briefcase'} size={20} color={'mediumaquamarine'}/>
-      <Text style={styles.h2}>
-        Työnantaja
-      </Text>
-    </View>
-    <Text>
-      {values.organizationDesc ? values.organizationDesc : ''}
-    </Text>
-    <Text style={{color: 'mediumaquamarine', paddingTop: 10}}>
-      {values.address}, {values.postalCode} {values.location}
-    </Text>
+    { renderOrganizationAndAddress(
+      values.organizationDesc, 
+      renderAddressBlock(values.address, values.postalCode, values.location)
+    ) }
   </View>
 )
 
@@ -192,7 +283,7 @@ const AdvertisementLayout = ({values}) => (
 
 const JobAdvertisement = () => {
   const [data, setData] = useState(jobAd);
-
+  
   return (
     <AdvertisementLayout values={data.jobAdvertisement} />
   )
---
+++ styles.jsx
@@ -0,0 +1,70 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+    container: {
+      paddingHorizontal: 10,
+      paddingBottom: 20,
+      paddingTop: 10
+    },
+    containerBright: {
+      backgroundColor: 'white',
+    },
+    containerDim: {
+      backgroundColor: 'azure',
+    },
+    row: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      paddingVertical: 3
+    },
+    button: {
+      paddingHorizontal: 10,
+      paddingVertical: 10,
+      borderRadius: 20,
+      backgroundColor: 'mediumaquamarine',
+      alignSelf: 'flex-start',
+      marginHorizontal: '1%',
+      marginBottom: 6,
+      minWidth: '33%',
+      textAlign: 'center',
+    },
+    buttonLabel: {
+      fontSize: 12,
+      fontWeight: '500',
+      color: 'black',
+      textAlign: 'center'
+    },
+    h1: {
+      textAlign: 'left',
+      marginBottom: 10,
+      fontSize: 24,
+    },
+    h2: {
+      textAlign: 'left',
+      fontSize: 20,
+      marginBottom: 10,
+    },
+    icon: {
+      paddingEnd: 10,
+      textAlignVertical: 'center',
+    },
+    iconButton: {
+      textAlignVertical: 'center',
+      padding: 5
+    },
+    rowButton: {
+      paddingTop: 20,
+      alignItems: 'stretch',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between'
+    },
+    viewToggle: {
+      color: 'mediumaquamarine',
+      paddingTop: 20,
+      textAlign: 'center',
+      fontWeight: 'bold'
+    }
+  });
+
+export default Styles;
