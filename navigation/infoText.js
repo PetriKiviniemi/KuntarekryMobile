49f0e104757b217a7a71e460274fa89c1e5f0f53
Elisa Kortela <kortela@protonmail.com>
2023-04-03 11:28:28 +0300

    Päivitetty info-välilehti

---
+++ navigation/infoText.js
@@ -0,0 +1,8 @@
+export default {
+  "intro" : "B-Kuntarekrystä-B löytyy tuhansia avoimia työpaikkoja kaikkialta Suomesta",
+  "forApplicants": "B-Työnhakijoille-B tarjoamme työvälineet työpaikkojen, sijaisuuksien ja keikkatöiden hakemiseen sekä tietoa työskentelystä kuntaorganisaatiossa ja hyvinvointialueilla.",
+  "forEmployers": "B-Työnantajille-B - kunnille, kaupungeille, hyvinvointialueille, kuntayhtymille ja   kuntien omistamille yrityksille - tarjoamme rekrytoinnin ohjelmisto- ja asiantuntijapalveluja, jotka sopivat ulkoiseen ja sisäiseen rekrytointiin sekä sijaisuuksien hallintaan.",
+  "contact": "Palautetta sivostosta voit lähettää osoitteeseen: U-tuki@kuntarekry.fi-U",
+  "dataStorage": "Sovellus tallentaa käyttäjän siihen syöttäviä tietoja, kuten käyttäjätiedot, hakutermit ja suosikeiksi merkityt työpaikkailmoitukset B-laitteen paikallismuistiin-B. Mitään näitä B-tietoja ei lähetetä verkon yli-B Kuntarekrylle tai kolmansille tahoille.",
+  "geolocation": "Jos käyttäjä haluaa hyödyntää B-paikannusta-B työpaikkailmoitusten haussa, tarvitsee sovellus luvan laitteen sijainnin määrittämiseen. B-Sijaintitietojen antaminen ei ole pakollista-B sovelluksen käyttämiselle. Käyttäjä voi milloi tahansa halutessaan muuttaa sovelluksen sijaintiasetuksia ja estää sovellusta käyttämästä tätä ominaisuutta."
+}
