6a96130fd566ed3cbddc33c68b6f0d57492f7547
Petri Kiviniemi <petri.kivi@hotmail.com>
2023-04-12 14:31:45 +0300

    Added tab navigation to profile screen

--- navigation/profile/profileScreen.jsx
+++ navigation/profile/profileScreen.jsx
@@ -1,28 +1,51 @@
-import React, {useEffect, useState, useRef} from 'react';
-import {Text, View, Button, TextInput, StyleSheet, Modal, Pressable, TouchableOpacity, ScrollView, LogBox} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import CheckBox from 'expo-checkbox'
-import {DatePicker} from "react-native-common-date-picker";
-import DropDownPicker from 'react-native-dropdown-picker';
+import React, {useEffect, useState} from 'react';
+import {Text, View, Button, TouchableOpacity, ScrollView, LogBox} from 'react-native';
 import Styles from '../../styles';
 import {profileStyles} from './profileStyles'
 
-import {storeValue, getValue, clearStorage, loginUser, createNewUser, getUserData, postUserData} from '../../utils/asyncstorage_utils';
+import {getValue, loginUser, createNewUser} from '../../utils/asyncstorage_utils';
 import LoginScreen from '../loginScreen';
-import {CalendarSelection} from './calendarSelection';
 import {DropdownMenu} from './dropdownMenu';
 import {DegreeOverlay} from './degreeOverlay';
-import { AddContentButton, SaveButton, CancelButton, TextInputImmutable, CheckBoxImmutable } from './profileMisc';
 import {PersonalInfo} from './personalInfo'
 import { AdditionalEducationOverlay } from './additionalEducationOverlay';
 import { WorkExperienceOverlay } from './workExperienceOverlay';
+import {ProfileTab} from './profileTab'
+import {WorkTab} from './workTab'
 
-export default function ProfileScreen({ navigation }) {
+const ProfileNavBar = (props) => {
+    return(
+            <View style={Styles.row2}>
+                <View style={profileStyles.profileTabContainer}>
+                <TouchableOpacity
+                    style={profileStyles.profileTabLeft}
+                    onPress={() => props.switchToTab("profile")}
+                >
+                    <Text style={{fontSize: 24}}>
+                        Profiili
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={profileStyles.profileTabRight}
+                    onPress={() => props.switchToTab("work")}
+                >
+                    <Text style={{fontSize: 24}}>
+                        Työt
+                    </Text>
+                </TouchableOpacity>
+                </View>
+            </View>
+    )
+}
+
+export default function ProfileScreen({navigation}) {
     //Start state
     const [loggedIn, setLoggedIn] = useState(false)
     
     //User data
     const [currentUser, setCurrentUser] = useState(null)
+
+    const [currentTab, setCurrentTab] = useState("profile")
 
     useEffect(() => {
         //Disable a specific console error. The hierarchy of the components is correct
@@ -58,7 +81,6 @@ export default function ProfileScreen({ navigation }) {
         })
     }, [])
 
-
     //TODO:: Replace with some real login function
     const getUserDataFromLogin = async (data) => {
         setLoggedIn(true)
@@ -73,71 +95,18 @@ export default function ProfileScreen({ navigation }) {
         }
     }
 
+    const switchToTab = (tabName) => {
+        setCurrentTab(tabName)
+    }
+
+
     if(!loggedIn)
         return <LoginScreen userdataCallback={getUserDataFromLogin}/>
-    return (
+    return(
         <View style={{flex: 1, paddingTop: 50}}>
-
-            <View style={Styles.row2}>
-                <View style={profileStyles.profileTabContainer}>
-                <TouchableOpacity style={profileStyles.profileTabLeft}>
-                    <Text style={{fontSize: 24}}>
-                        Profiili
-                    </Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={profileStyles.profileTabRight}>
-                    <Text style={{fontSize: 24}}>
-                        Työt
-                    </Text>
-                </TouchableOpacity>
-                </View>
-            </View>
-
-            <View style={Styles.row2}>
-                <Text style={profileStyles.profileTextField}>
-                    Omaa hakijaprofiiliasi voit käyttää pohjana työhakemuksissasi.
-                    Oman profiilisi tiedot eivät näy työnantajalle,
-                    he näkevät vain heille lähetetyt työhakemukset.
-                </Text>
-                <Text style={profileStyles.profileTextField}>
-                    Jos päivität perustietojasi
-                    (nimi, syntymäaika, sukupuoli, puhelin, sähköposti),
-                    ne päivittyvät automaattisesti kaikille aikaisemmin jättämillesi hakemuksille.
-                </Text>
-                <Text style={profileStyles.profileTextField}>
-                    Mikäli päivität profiilissasi tutkinnot,
-                    lisä- ja täydennyskoulutus ja työkokemustietoja,
-                    ne eivät päivity automaattisesti aikaisemmin jätetyille hakemuksille.
-                </Text>
-            </View>
-
-            <ScrollView style={profileStyles.dropdownsContainer}>
-            <View style={profileStyles.dropdownsWrapper}>
-
-                <DropdownMenu
-                    title="PERUSTIEDOT"
-                    content={<PersonalInfo user={currentUser}/>}
-                />
-                <DropdownMenu
-                    title="TUTKINNOT"
-                    content={<DegreeOverlay user={currentUser}/>}
-                />
-
-                <DropdownMenu
-                    title="LISÄ- JA TÄYDENNYSKOULUTUS"
-                    content={<AdditionalEducationOverlay user={currentUser}/>}
-                />
-
-                <DropdownMenu
-                    title="TYÖKOKEMUS"
-                    content={<WorkExperienceOverlay user={currentUser}/>}
-                />
-            </View>
-            </ScrollView>
-
-            <Button onPress={() => setLoggedIn(false)} title="Logout">
-            </Button>
-
+            <ProfileNavBar switchToTab={switchToTab}/>
+            {currentTab == 'profile' ? <ProfileTab currentUser={currentUser}/> : <WorkTab currentUser={currentUser}/>}
+            <Button onPress={() => setLoggedIn(false)} title="Logout"/>
         </View>
-    );
+    )
 }
---
+++ navigation/profile/profileTab.jsx
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from 'react';
+import {Text, View, Button, TouchableOpacity, ScrollView, LogBox} from 'react-native';
+import Styles from '../../styles';
+import {profileStyles} from './profileStyles'
+
+import {getValue, loginUser, createNewUser} from '../../utils/asyncstorage_utils';
+import LoginScreen from '../loginScreen';
+import {DropdownMenu} from './dropdownMenu';
+import {DegreeOverlay} from './degreeOverlay';
+import {PersonalInfo} from './personalInfo'
+import { AdditionalEducationOverlay } from './additionalEducationOverlay';
+import { WorkExperienceOverlay } from './workExperienceOverlay';
+
+export const ProfileTab = (props) => {
+    return (
+        <View style={{flex: 1}}>
+
+            <View style={Styles.row2}>
+                <Text style={profileStyles.profileTextField}>
+                    Omaa hakijaprofiiliasi voit käyttää pohjana työhakemuksissasi.
+                    Oman profiilisi tiedot eivät näy työnantajalle,
+                    he näkevät vain heille lähetetyt työhakemukset.
+                </Text>
+                <Text style={profileStyles.profileTextField}>
+                    Jos päivität perustietojasi
+                    (nimi, syntymäaika, sukupuoli, puhelin, sähköposti),
+                    ne päivittyvät automaattisesti kaikille aikaisemmin jättämillesi hakemuksille.
+                </Text>
+                <Text style={profileStyles.profileTextField}>
+                    Mikäli päivität profiilissasi tutkinnot,
+                    lisä- ja täydennyskoulutus ja työkokemustietoja,
+                    ne eivät päivity automaattisesti aikaisemmin jätetyille hakemuksille.
+                </Text>
+            </View>
+
+            <ScrollView style={profileStyles.dropdownsContainer}>
+            <View style={profileStyles.dropdownsWrapper}>
+
+                <DropdownMenu
+                    title="PERUSTIEDOT"
+                    content={<PersonalInfo user={props.currentUser}/>}
+                />
+                <DropdownMenu
+                    title="TUTKINNOT"
+                    content={<DegreeOverlay user={props.currentUser}/>}
+                />
+
+                <DropdownMenu
+                    title="LISÄ- JA TÄYDENNYSKOULUTUS"
+                    content={<AdditionalEducationOverlay user={props.currentUser}/>}
+                />
+
+                <DropdownMenu
+                    title="TYÖKOKEMUS"
+                    content={<WorkExperienceOverlay user={props.currentUser}/>}
+                />
+            </View>
+            </ScrollView>
+        </View>
+    );
+}
